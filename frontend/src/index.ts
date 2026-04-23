e928696db068b8a6f598b0d73787d7bf2c990b5a
spellsa <spelling.import@gmail.com>
2026-04-24 00:52:05 +0900

    文字数カウントが動いた

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,12 +3,19 @@ import { socket } from "./socket.ts";
 const form = document.getElementById("chatForm") as HTMLFormElement;
 const input = document.getElementById("chatText") as HTMLInputElement;
 const timeline = document.getElementById("timeline") as HTMLUListElement;
+const charCount = document.getElementById("charCount") as HTMLSpanElement;
+
+input.addEventListener("input", () => {
+  const length = input.value.length;
+  charCount.textContent = `${length}/50`;
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
     input.value = "";
+    charCount.textContent = `0/50`;
   }
 });
 
